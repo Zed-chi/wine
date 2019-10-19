303e368beba6b757f59164b0e527d20797353d4a
zed <zed@mail.ru>
2019-10-19 12:59:10 +0300

    upd

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-from text_parser import get_wine_groups_from_text
+from text_parser import get_wine_by_categories
 import datetime
 
 
 since_year = 1920
 current_year = datetime.datetime.now().year
 company_age = current_year - since_year
-wine_groups = get_wine_groups_from_text()
+wine_by_categories = get_wine_by_categories()
 
 env = Environment(
     loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
 )
 template = env.get_template("template.html")
 rendered_page = template.render(
-    company_age=company_age, wine_groups=wine_groups
+    company_age=company_age, wine_categories=wine_by_categories
 )
 
 with open("index.html", "w", encoding="utf8") as file:
