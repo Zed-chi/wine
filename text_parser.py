e897a8b87163c6b66ca0f1d8e0cf70f10dc9eb20
zed <zed@mail.ru>
2019-10-19 12:59:34 +0300

    rewrite code

--- text_parser.py
+++ text_parser.py
@@ -2,53 +2,69 @@ import re
 from collections import namedtuple
 
 
-class Empty:
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return ""
+def get_text(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
 
 
-default_textfile = "./wine.txt"
-wine = namedtuple("Wine", ["name", "sort", "price", "image"])
-group = namedtuple("Group", ["title", "items"])
-patterns = {
-    "title": re.compile("(?<=# ).+"),
-    "fragment": re.compile("(?<=# )[а-яА-Яa-zA-Z0-9\n :\/_.]+"),
-    "name": re.compile("(?<=Название: ).+"),
-    "sort": re.compile("(?<=Сорт: ).+"),
-    "price": re.compile("(?<=Цена: ).+"),
-    "image": re.compile("(?<=Картинка: ).+"),
-}
+def find(pattern, text):
+    res = re.search(re.compile(pattern), text)
+    if res:
+        return res[0]
+    return None
 
 
-def get_file_data(filename):
-    if filename:
-        with open(filename, "r", encoding="utf-8") as file:
-            return file.read()
+def get_titles_and_category_blocks(text):
+    arr = text.split("\n\n\n")
+    titles = list(map(lambda x: x.replace("# ", ""), arr[::2]))
+    cat_blocks = list(
+        map(
+            lambda a: a.strip(),
+            arr[1::2]
+        )
+    )
+    return titles, cat_blocks
 
 
-def get_wine_items_from_text(text, mock=Empty()):
-    names = re.findall(patterns["name"], text) or mock
-    sorts = re.findall(patterns["sort"], text) or mock
-    prices = re.findall(patterns["price"], text) or mock
-    images = re.findall(patterns["image"], text) or mock
-    return list(map(lambda tup: wine(*tup), zip(names, sorts, prices, images)))
+def get_wine_blocks(category_block):
+    return category_block.split("\n\n")
 
 
-def get_wine_groups_from_text(
-    text=get_file_data(default_textfile), patterns=patterns
-):
-    group_items = []
-    titles = re.findall(patterns["title"], text)
-    fragments = re.findall(patterns["fragment"], text)
-    for fragment in fragments:
-        wine_items = get_wine_items_from_text(fragment)
-        group_items.append(wine_items)
-    wine_groups = list(map(lambda tup: group(*tup), zip(titles, group_items)))
-    return wine_groups
+def get_dict_from_wine_block(wine_block):
+    wine = {
+        "name": find("(?<=Название: ).+", wine_block),
+        "sort": find("(?<=Сорт: ).+", wine_block),
+        "price": find("(?<=Цена: ).+", wine_block),
+        "image": find("(?<=Картинка: ).+", wine_block),
+        "discount": find("Выгодное предложение", wine_block),
+    }
+    return wine
 
 
-if __name__ == "__main__":
-    print(get_ntuple_from_text())
+def get_wine_items_from_category_block(cat_block):
+    wine_blocks = get_wine_blocks(cat_block)
+    wine_items = list(
+        map(
+            lambda a:get_dict_from_wine_block(a),
+            wine_blocks
+        )
+    )
+    return wine_items
+
+
+def get_wine_by_categories(textfile="./wine.txt"):
+    categorized_wine = namedtuple("wine", ["title", "items"])
+    text = get_text(textfile)
+    titles, cat_blocks = get_titles_and_category_blocks(text)
+    wine_by_category = list(
+        map(
+            lambda cat: get_wine_items_from_category_block(cat),
+            cat_blocks
+        )
+    )
+    return list(
+        map(
+            lambda a: categorized_wine(*a),
+            list(zip(titles, wine_by_category))
+        )
+    )
